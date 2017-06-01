31f7b1265c32e6699dc94806c510659778c5b7bc
Gabriel Alejandro De la Vega Vaughan <alejandro7mm@gmail.com>
2017-05-31 19:42:07 -0500

    BT_16 Predict Projectile Landing Point

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
 
@@ -11,6 +12,12 @@ class BATTLETANK_API ATank : public APawn
 	GENERATED_BODY()
 public:
     void AimAt(FVector HitLocation);
+    
+protected:
+    UTankAimingComponent* TankAimingComponent = nullptr;
+    
+    UFUNCTION(BlueprintCallable, Category = Setup)
+    void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
     
 private:
     
@@ -25,4 +32,7 @@ private:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    
+    UPROPERTY(EditAnywhere, Category = Firing)
+    float LaunchSpeed = 100000; // TODO find sensible default
 };
